de7fddd7fa5560dce714bf5d73caf4266647757a
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-01-31 23:00:54 +0500

    add fixtures, add api and index

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import getDiff from '../lib/api.js';
 
 const program = new Command();
 
@@ -8,4 +9,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  });
 program.parse(process.argv);
