970158c6b479b1cf87734f411ec150d4f7cc2d34
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-02-03 21:17:58 +0500

    add fixtures(yml), add tests(yml check), add parser.js(file extension comprasion logic)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,6 +9,8 @@ let jsonBefore;
 let jsonAfter;
 let correctResult;
 let wrongResult;
+let ymlBefore;
+let ymlAfter;
 
 beforeAll(() => {
   const __filename = fileURLToPath(import.meta.url);
@@ -17,13 +19,18 @@ beforeAll(() => {
   const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
   jsonBefore = getFixturePath('flatFile1.json');
   jsonAfter = getFixturePath('flatFile2.json');
+  ymlBefore = getFixturePath('flatFile1.yml');
+  ymlAfter = getFixturePath('flatFile2.yml');
+
   correctResult = readFile('correctResultFlat.txt');
   wrongResult = readFile('wrongResultFlat.txt');
 });
 
-test('correct difference flat json', () => {
+test('correct difference of the flat files', () => {
   expect(genDiff(jsonBefore, jsonAfter)).toBe(correctResult);
+  expect(genDiff(ymlBefore, ymlAfter)).toBe(correctResult);
 });
-test('wrong difference flat json', () => {
+test('wrong difference of the flat files', () => {
   expect(genDiff(jsonBefore, jsonAfter)).not.toBe(wrongResult);
+  expect(genDiff(ymlBefore, ymlAfter)).not.toBe(wrongResult);
 });
