892b5e24e6a928cdea9c6db6d9b86d9def83b88e
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-02-16 18:59:59 +0500

    fix generareDiff.js and change stylish.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+// import stylish from '../lib/formatters/stylish.js';
 import genDiff from '../lib/index.js';
 
 const program = new Command();
@@ -8,7 +9,7 @@ program
   .version('0.1.0')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const resultString = genDiff(filepath1, filepath2);
     console.log(resultString);
