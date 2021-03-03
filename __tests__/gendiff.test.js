c00584ea6b9cba9e90558c36b27703e28b6e155d
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-03-03 14:01:26 +0500

    fix gendiff.test.js (remove mutations, add test.each)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,43 +1,45 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import { beforeAll, test, expect } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import genDiff from '../lib/index.js';
-
-let jsonBefore;
-let jsonAfter;
-let ymlBefore;
-let ymlAfter;
-let stylishCorrectResult;
-let plainCorrectResult;
-let jsonCorrectResult;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-beforeAll(() => {
-  jsonBefore = getFixturePath('before.json');
-  jsonAfter = getFixturePath('after.json');
-  ymlBefore = getFixturePath('before.yml');
-  ymlAfter = getFixturePath('after.yml');
+const jsonBefore = getFixturePath('before.json');
+const jsonAfter = getFixturePath('after.json');
+const ymlBefore = getFixturePath('before.yml');
+const ymlAfter = getFixturePath('after.yml');
 
-  stylishCorrectResult = readFile('stylishResult.txt');
-  plainCorrectResult = readFile('plainResult.txt');
-  jsonCorrectResult = readFile('jsonResult.txt');
-});
+const plainCorrectResult = readFile('plainResult.txt');
+const stylishCorrectResult = readFile('stylishResult.txt');
+const jsonCorrectResult = readFile('jsonResult.txt');
 
-test('correct difference of the nested files (stylish)', () => {
-  expect(genDiff(jsonBefore, jsonAfter)).toBe(stylishCorrectResult);
-  expect(genDiff(ymlBefore, ymlAfter)).toBe(stylishCorrectResult);
-  expect(genDiff(jsonBefore, ymlAfter)).toBe(stylishCorrectResult);
-});
-test('correct difference of the nested files (plain)', () => {
-  expect(genDiff(jsonBefore, jsonAfter, 'plain')).toBe(plainCorrectResult);
-  expect(genDiff(jsonBefore, ymlAfter, 'plain')).toBe(plainCorrectResult);
-});
-test('correct difference of the nested files (json)', () => {
-  expect(genDiff(jsonBefore, jsonAfter, 'json')).toBe(jsonCorrectResult);
-  expect(genDiff(jsonBefore, ymlAfter, 'json')).toBe(jsonCorrectResult);
+describe('correct difference of the nested files', () => {
+  test.each([
+    [jsonBefore, jsonAfter, stylishCorrectResult],
+    [ymlBefore, ymlAfter, stylishCorrectResult],
+    [jsonBefore, ymlAfter, stylishCorrectResult],
+  ])('stylish', (before, after, result) => {
+    expect(genDiff(before, after)).toBe(result);
+  });
+
+  test.each([
+    [jsonBefore, jsonAfter, plainCorrectResult],
+    [ymlBefore, ymlAfter, plainCorrectResult],
+    [jsonBefore, ymlAfter, plainCorrectResult],
+  ])('plain', (before, after, result) => {
+    expect(genDiff(before, after, 'plain')).toBe(result);
+  });
+
+  test.each([
+    [jsonBefore, jsonAfter, jsonCorrectResult],
+    [ymlBefore, ymlAfter, jsonCorrectResult],
+    [jsonBefore, ymlAfter, jsonCorrectResult],
+  ])('json', (before, after, result) => {
+    expect(genDiff(before, after, 'json')).toBe(result);
+  });
 });
